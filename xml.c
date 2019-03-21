09a7193a8776cca8054cc8538dc178358593b355
Ben Hanna <benpaulhanna@gmail.com>
2019-03-21 14:32:02 -0400

    Refactored XML parsing.

--- xml.c
+++ xml.c
@@ -7,19 +7,21 @@
 #include "sprite.h"
 
 #define START_ELEMENT 1
+#define TEXT_ELEMENT 3
 #define END_ELEMENT 15
 
 #define SPRITE_FILE "sprites.xml"
 
-int named(char*, char*);
-int read_sprite(xmlTextReaderPtr, struct Sprite*);
+struct GameState {
+  struct SpriteNode *sprite_head;
+};
 
 int main(int argc, char *argv[]) {
+  struct GameState *state = calloc(1, sizeof(struct GameState));
+  
   xmlTextReaderPtr reader = xmlNewTextReaderFilename(SPRITE_FILE);
   
   if (reader != NULL) {
-    struct SpriteNode *head = NULL, *current, *node;
-    
     while (xmlTextReaderRead(reader)) {
       int type = xmlTextReaderNodeType(reader);
       char *name = xmlTextReaderName(reader);
@@ -27,15 +29,39 @@ int main(int argc, char *argv[]) {
       switch (type) {
         case START_ELEMENT:
           if (!strcmp(name, "sp:sprite")) {
-            node = calloc(1, sizeof(struct SpriteNode));
-
-            if (read_sprite(reader, &node->data)) {
-              if (head == NULL) {
-                current = head = node;
-              } else {
-                current = current->next = node;
-              }
-            }
+            struct SpriteNode *node = calloc(1, sizeof(struct SpriteNode));
+            node->next = state->sprite_head;
+            state->sprite_head = node; 
+          }
+          
+          if (!strcmp(name, "sp:x")) {
+            char *value = xmlTextReaderReadString(reader);
+            state->sprite_head->data.x = atoi(value);
+            xmlFree(value);
+          }
+          
+          if (!strcmp(name, "sp:y")) {
+            char *value = xmlTextReaderReadString(reader);
+            state->sprite_head->data.y = atoi(value);
+            xmlFree(value);
+          }
+          
+          if (!strcmp(name, "an:frame_width")) {
+            char *value = xmlTextReaderReadString(reader);
+            state->sprite_head->data.animation.frame_w = atoi(value);
+            xmlFree(value);
+          }
+          
+          if (!strcmp(name, "an:frame_height")) {
+            char *value = xmlTextReaderReadString(reader);
+            state->sprite_head->data.animation.frame_h = atoi(value);
+            xmlFree(value);
+          }
+          
+          if (!strcmp(name, "an:frame_span")) {
+            char *value = xmlTextReaderReadString(reader);
+            state->sprite_head->data.animation.frame_span = atoi(value);
+            xmlFree(value);
           }
           
           break;
@@ -46,85 +72,6 @@ int main(int argc, char *argv[]) {
     
     xmlFreeTextReader(reader);
   }
-}
-
-int read_animation(xmlTextReaderPtr reader, struct Animation *animation) {
-  int ret = 0;
   
-  while (!ret) {
-    xmlTextReaderRead(reader);
-    
-    int type = xmlTextReaderNodeType(reader);
-    char *name = xmlTextReaderName(reader);
-    char *value;
-    
-    switch (type) {
-      case START_ELEMENT:
-        value = xmlTextReaderReadString(reader);
-      
-        if (!strcmp(name, "an:frame_width")) {
-          animation->frame_w = atoi(value);
-        }
-        
-        if (!strcmp(name, "an:frame_height")) {
-          animation->frame_h = atoi(value);
-        }
-        
-        if (!strcmp(name, "an:frame_span")) {
-          animation->frame_span = atoi(value);
-        }
-        
-        xmlFree(value);
-        break;
-        
-      case END_ELEMENT:
-        if (!strcmp(name, "an:animation")) {
-          ret = 1;
-        }
-    }
-    
-    xmlFree(name);
-  }
-  
-  return ret;
-}
-
-int read_sprite(xmlTextReaderPtr reader, struct Sprite *sprite) {
-  int ret = 0;
-  
-  while (!ret) {
-    xmlTextReaderRead(reader);
-    
-    int type = xmlTextReaderNodeType(reader);
-    char *name = xmlTextReaderName(reader);
-    
-    switch (type) {
-      case START_ELEMENT:
-        if (!strcmp(name, "an:animation")) {
-          read_animation(reader, &sprite->animation);
-        } else {
-          char *value = xmlTextReaderReadString(reader);
-          
-          if (!strcmp(name, "sp:x")) {
-            sprite->x = atoi(value);
-          }
-          
-          if (!strcmp(name, "sp:y")) {
-            sprite->y = atoi(value);
-          }
-          
-          xmlFree(value);
-        }
-        break;
-        
-      case END_ELEMENT:
-        if (!strcmp(name, "sp:sprite")) {
-          ret = 1;
-        }
-    }
-    
-    xmlFree(name);
-  }
-  
-  return ret;
+  free(state);
 }
