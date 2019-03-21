e857fc2ee616472829cb53df2229a6fb8d2c3fe7
Ben Hanna <benpaulhanna@gmail.com>
2019-03-21 08:41:39 -0400

    Improved sprite loading.

--- xml.c
+++ xml.c
@@ -12,6 +12,7 @@
 #define SPRITE_FILE "sprites.xml"
 
 int named(char*, char*);
+int read_sprite(xmlTextReaderPtr, struct Sprite*);
 
 int main(int argc, char *argv[]) {
   xmlTextReaderPtr reader = xmlNewTextReaderFilename(SPRITE_FILE);
@@ -20,62 +21,92 @@ int main(int argc, char *argv[]) {
     struct SpriteNode *head = NULL, *current, *node;
     
     while (xmlTextReaderRead(reader)) {
-      int node_type = xmlTextReaderNodeType(reader);
-      char *node_name = xmlTextReaderName(reader);
+      int type = xmlTextReaderNodeType(reader);
+      char *name = xmlTextReaderName(reader);
       
-      switch (node_type) {
+      switch (type) {
         case START_ELEMENT:
-          if (!strcmp(node_name, "sp:sprite")) {
+          if (!strcmp(name, "sp:sprite")) {
             node = calloc(1, sizeof(struct SpriteNode));
-          }
-          
-          if (xmlTextReaderHasAttributes(reader)) {
-            while(xmlTextReaderMoveToNextAttribute(reader)) {
-              char *attr_name = xmlTextReaderName(reader); 
-              char *attr_value = xmlTextReaderValue(reader);
-              
-              if (!strcmp(node_name, "sp:sprite")) {
-                if (!strcmp(attr_name, "x")) {
+            
+            if (xmlTextReaderHasAttributes(reader)) {
+              while(xmlTextReaderMoveToNextAttribute(reader)) {
+                char *attr_name = xmlTextReaderName(reader); 
+                char *attr_value = xmlTextReaderValue(reader);
+                
+                if (!strcmp(name, "x")) {
                   node->data.x = atoi(attr_value);
                 }
-                if (!strcmp(attr_name, "y")) {
+                if (!strcmp(name, "y")) {
                   node->data.y = atoi(attr_value);
                 }
+
+                xmlFree(attr_name);
+                xmlFree(attr_value);
               }
-              
-              if (!strcmp(node_name, "sp:animation")) {
-                if (!strcmp(attr_name, "frame_width")) {
-                  node->data.animation.frame_w = atoi(attr_value);
-                }
-                if (!strcmp(attr_name, "frame_height")) {
-                  node->data.animation.frame_h = atoi(attr_value);
-                }
-                if (!strcmp(attr_name, "frame_span")) {
-                  node->data.animation.frame_span = atoi(attr_value);
-                }
+            }
+            
+            if (read_sprite(reader, &node->data)) {
+              if (head == NULL) {
+                current = head = node;
+              } else {
+                current = current->next = node;
               }
-              
-              xmlFree(attr_name);
-              xmlFree(attr_value);
             }
           }
           
-          break;
-          
-        case END_ELEMENT:
-          if (!strcmp(node_name, "sp:sprite")) {
-            if (head == NULL) {
-              current = head = node;
-            } else {
-              current = current->next = node;
-            }
-          }
           break;
       }
       
-      xmlFree(node_name);
+      xmlFree(name);
     }
     
     xmlFreeTextReader(reader);
   }
+}
+
+int read_sprite(xmlTextReaderPtr reader, struct Sprite *sprite) {
+  int ret = 0;
+  
+  while (!ret) {
+    xmlTextReaderRead(reader);
+    
+    int type = xmlTextReaderNodeType(reader);
+    char *name = xmlTextReaderName(reader);
+    
+    switch (type) {
+      case START_ELEMENT:
+        if (!strcmp(name, "sp:animation")) {
+          if (xmlTextReaderHasAttributes(reader)) {
+              while(xmlTextReaderMoveToNextAttribute(reader)) {
+                char *attr_name = xmlTextReaderName(reader); 
+                char *attr_value = xmlTextReaderValue(reader);
+                
+                if (!strcmp(attr_name, "frame_width")) {
+                  sprite->animation.frame_w = atoi(attr_value);
+                }
+                if (!strcmp(attr_name, "frame_height")) {
+                  sprite->animation.frame_h = atoi(attr_value);
+                }
+                if (!strcmp(attr_name, "frame_span")) {
+                  sprite->animation.frame_span = atoi(attr_value);
+                }
+                
+                xmlFree(attr_name);
+                xmlFree(attr_value);
+              }
+            }
+        }
+        break;
+        
+      case END_ELEMENT:
+        if (!strcmp(name, "sp:sprite")) {
+          ret = 1;
+        }
+    }
+    
+    xmlFree(name);
+  }
+  
+  return ret;
 }
